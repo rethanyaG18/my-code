59725789d5d98191b65f5eaaa167fc404ddea902
RETHANYA G <rethanyaguruvadivel2006@gmail.com>
2026-01-07 18:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (21.02%) - LeetSync

---
+++ 1260-day-of-the-year/day-of-the-year.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dayOfYear(string date) {
+        int y = stoi(date.substr(0, 4));
+        int m = stoi(date.substr(5, 2));
+        int d = stoi(date.substr(8, 2));
+
+        int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        for (int i = 0; i < m - 1; ++i)
+            d += daysInMonth[i];
+
+        if (m > 2 && ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0)))
+            ++d;
+
+        return d;
+    }
+};
